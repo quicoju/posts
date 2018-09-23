a65a403ffabbb10939c1efb8431609d6d82a52b4
Francisco Jurado <fjurado@cimat.mx>
2018-09-23 20:52:39 +0200

    Initial commit of posts.

---
+++ js/my.js
@@ -0,0 +1,39 @@
+function prev( directive, n ) {
+    var text = "&lt; prev" ;
+    return directive.indexOf("prev") >= 0 && n > 0 ?
+        link( "/" + (n-1), text ) : text;
+}
+
+function next( directive, n ) {
+    var text = "next &gt;";
+    return directive.indexOf("next") >= 0 ?
+        link( "/" + (n+1), text ) : text;
+}
+
+function link( path, text ) {
+    var loc = window.location;
+    var url = "http://" + loc.hostname + "/posts" + path;
+    return "<a href=\""+ url +"\">" + text + "</a>";
+}
+
+function do_navigation_menu( ) {
+    var menu_div = document.getElementsByClassName("navigator");
+
+    if ( menu_div[0] ) {
+        var loc   = window.location;
+        var match = loc.pathname.match( /\/posts\/([\d]+)/ );
+        var post  = match ? parseInt( match[1] ) : 0;
+        
+        var directive = menu_div[0].innerHTML;
+        menu_div[0].innerHTML
+            = "<p>"
+            + prev( directive, post ) + " | "
+            + link("/", "home")       + " | "
+            + next( directive, post )
+            + "</p>";
+    }
+}
+
+// draw the navigation menu
+// ========================
+window.onload = do_navigation_menu;
