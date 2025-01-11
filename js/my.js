9525cad9ac9ef55c838cb92671fc88cc1ff95dac
Francisco Jurado <fjurado@cimat.mx>
2025-01-11 08:15:04 +0100

    add support for a second navigator

--- js/my.js
+++ js/my.js
@@ -17,17 +17,19 @@ function link( path, text ) {
 function do_navigation_menu( ) {
     var menu_div = document.getElementsByClassName("navigator");
 
-    if ( menu_div[0] ) {
-        var loc   = window.location;
-        var match = loc.pathname.match( /\/posts\/([\d]+)/ );
-        var post  = match ? parseInt( match[1] ) : 0;
-        var directive = menu_div[0].innerHTML;
-        menu_div[0].innerHTML
-            = "<p>"
-            + prev( directive, post ) + " | "
-            + link("index", "index")  + " | "
-            + next( directive, post )
-            + "</p>";
+    for (let i = 0; i < 2; i++) {
+        if ( menu_div[i] ) {
+            var loc   = window.location;
+            var match = loc.pathname.match( /\/posts\/([\d]+)/ );
+            var post  = match ? parseInt( match[1] ) : 0;
+            var directive = menu_div[i].innerHTML;
+            menu_div[i].innerHTML
+                = "<p>"
+                + prev( directive, post ) + " | "
+                + link("index", "index")  + " | "
+                + next( directive, post )
+                + "</p>";
+        }
     }
 }
 
