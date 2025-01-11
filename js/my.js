7afe95802422ab906643b2b0cd5c4242c5292c15
Francisco Jurado <fjurado@cimat.mx>
2025-01-11 07:51:36 +0100

    use relative paths and links to "html" documents

--- js/my.js
+++ js/my.js
@@ -1,19 +1,17 @@
 function prev( directive, n ) {
     var text = "&lt; prev" ;
-    return n > 0 || directive.indexOf("prev") >= 0 ?
-        link( "/" + (n-1), text ) : text;
+    return n > 1 || directive.indexOf("prev") >= 0 ?
+        link((n-1), text) : text;
 }
 
 function next( directive, n ) {
     var text = "next &gt;";
     return directive.indexOf("next") >= 0 ?
-        link( "/" + (n+1), text ) : text;
+        link((n+1), text) : text;
 }
 
 function link( path, text ) {
-    var loc = window.location;
-    var url = "http://" + loc.hostname + "/posts" + path;
-    return "<a href=\""+ url +"\">" + text + "</a>";
+    return `<a href="./${path}.html">${text}</a>`;
 }
 
 function do_navigation_menu( ) {
@@ -23,12 +21,11 @@ function do_navigation_menu( ) {
         var loc   = window.location;
         var match = loc.pathname.match( /\/posts\/([\d]+)/ );
         var post  = match ? parseInt( match[1] ) : 0;
-        
         var directive = menu_div[0].innerHTML;
         menu_div[0].innerHTML
             = "<p>"
             + prev( directive, post ) + " | "
-            + link("/", "home")       + " | "
+            + link("index", "index")  + " | "
             + next( directive, post )
             + "</p>";
     }
